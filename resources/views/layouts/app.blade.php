3953d683e502caa3ccefce44fd53e7c7286acf97
Aiza Pacia <101613589+aizapacia@users.noreply.github.com>
2022-09-22 17:27:14 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,12 +16,10 @@
 
 
     <!-- plugins -->
-    <link rel="stylesheet" href="{{url('/template/plugins/bootstrap/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{url('/template/plugins/themify-icons/themify-icons.css') }}">
-
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!-- Main Stylesheet -->
-    <link href="{{url('/template/css/style.css') }}" rel="stylesheet">
+    <link href="/template/css/style.css" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -46,7 +44,8 @@
                     @endif
                     @else
                     <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navigation">
-                        <i class="ti-align-right h4 text-dark"></i></button>
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                     <div class="collapse navbar-collapse text-center" id=navigation>
                         <ul class="navbar-nav mx-auto align-items-center">
                             <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
@@ -95,15 +94,12 @@
         @yield('scripts')
 
         @livewireScripts
-        <!-- plugins -->
-        <script src="{{ url('/template/plugins/jQuery/jquery.min.js') }}"></script>
-        <script src="{{ url('/template/plugins/bootstrap/bootstrap.min.js') }}"></script>
-        <script src="{{ url('/template/plugins/masonry/masonry.min.js') }}"></script>
-        <script src="{{ url('/template/plugins/clipboard/clipboard.min.js') }}"></script>
-        <script src="{{ url('/template/plugins/match-height/jquery.matchHeight-min.js') }}"></script>
-
         <!-- Main Script -->
         <script src="{{ url('/template/js/script.js') }}"></script>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     </div>
 </body>
